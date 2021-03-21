d84d645e956ab0296278374b2821d204046e1315
Saba Lapanashvili <saba.lapanashvili15@gmail.com>
2021-03-21 15:46:00 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ProductProvider } from "./context";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>,
   document.getElementById('root')
 );
 
